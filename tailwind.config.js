ca4737faa44c4f2eb49672d12c83383b2041fcb7
Dominic Ranghiasci <dominicranghiasci@MacBook-Pro.local>
2025-04-22 21:43:21 -0500

    5379487539842375398420932485873942390848574934093284

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './app/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'class', // <-- enables class-based dark mode
-    theme: {
-      extend: {
-        // optional: extend colors, spacing, fonts here
-      },
-    },
-    plugins: [],
-  }
-  
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
